741e429febe47316474c34b264ce75427fad7158
MukhethwaSE <netshitukamukhethwa@gmail.com>
2023-10-09 21:23:34 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,14 @@
  */
 int main(void)
 {
-	char c = 'a';
-	char d = 'A';
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-	while (c <= 'z')
+	for (i = 0; i < 52; i++)
 	{
-		putchar(c);
-		c++;
-	}
-	while (d <= 'z')
-	{
-		putchar(d);
-		d++;
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
+
 }
