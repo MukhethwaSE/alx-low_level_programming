785999f99c85825e615d48dc20e6be4de2aee1e0
MukhethwaSE <netshitukamukhethwa@gmail.com>
2023-10-09 17:18:46 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The program uses function printf to print a line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}	
