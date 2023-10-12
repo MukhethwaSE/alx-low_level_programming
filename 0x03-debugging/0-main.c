d4606511f32b80c74ff60ccbba1b6a7bb0d2cbd4
MukhethwaSE <netshitukamukhethwa@gmail.com>
2023-10-12 04:43:52 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - The program tests if an interger is positive or negative
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
+
